59150e2e939df8b0d55e08e6a99182e6028c721f
VAOkolo <vincentaokolo@outlook.com>
2022-06-29 15:48:04 +0100

    changes

--- client/index.js
+++ client/index.js
@@ -28,7 +28,24 @@ document.addEventListener("DOMContentLoaded", function () {
 /* <--------------------  Post comments  --------------------->*/
   let parentSection = document.querySelector('.parentsection')
   
-  parentSection.addEventListener('click', printComment)
+  // parentSection.addEventListener('click', printComment)
+  // parentSection.addEventListener('click', updateReaction)
+  // parentSection.addEventListener('click', (e) => {
+      //if x printComment
+      //else if y updateReaction
+      //else do nothing
+      // e.preventDefault();
+      // console.log(e);
+
+    // printComment
+    // updateReaction
+  // })
+
+  let submitButton = document.querySelector('.submitbuttonpost')
+  console.log(submitButton);
+  submitButton.addEventListener('click', printComment);
+
+
   
 function printComment(e){
 
@@ -36,19 +53,21 @@ function printComment(e){
     e.preventDefault();
     // console.log(e);
     let submitButton = e.path[0].id;
-    console.log(typeof submitButton);
+    // console.log(submitButton);
     let postid = submitButton.slice(0, submitButton.indexOf('-'))
     let comment = e.path[1].childNodes[0].value;
-    console.log(postid)
+    // console.log(comment);
+    // console.log(postid)
 
-    if(submitButton != "" && postid != "" && comment != ""){
+    //add undefined after ""
+    if(submitButton != "" && postid != "" && comment != "" && submitButton != undefined && postid != undefined && comment != undefined){
     // console.log(postid);
 
         const url = `http://localhost:5000/posts/${postid}/comment`  // The url for post req to be sent to
-        console.log(url);
+        // console.log(url);
         let jsonData = {}
         jsonData.content = comment;
-        console.log(jsonData)
+        // console.log(jsonData)
         // jsonData.date = new Date();
 
       const options = {
@@ -65,12 +84,13 @@ function printComment(e){
       });
 
       // populate section with dummy data
+      if(comment != ""){}
       let ul = document.getElementById(`${postid}-commentul`);
-      console.log(ul);
+      // console.log(ul);
       let li = document.createElement('li');
-      console.log(comment);
+      // console.log(comment);
       li.textContent = comment;
-      console.log(li);
+      // console.log(li);
       ul.appendChild(li);
 
 
@@ -126,7 +146,7 @@ function appendResult(itemData) {
     ul.appendChild(li);
   }
   //append ul full of li comments as child element of comment container
-  console.log(ul);
+  // console.log(ul);
   commentContainer.appendChild(ul);
 
   // container.setAttribute('class', 'searchresult');
@@ -137,15 +157,19 @@ submitButton.setAttribute('type', 'submit');
 
   // console.log(itemData.id)
   content.textContent = itemData.content;
-  const emojiDiv = document.createElement("div");
+  const emojiDiv = document.createElement("section");
   const likeBtn = document.createElement("button");
   const likes = document.createElement("p");
+  const dislikeBtn = document.createElement('button');
+  const dislikes = document.createElement("p");
+  const loveBtn = document.createElement('button');
+  const love = document.createElement("p");
 
   // container.setAttribute('class', 'searchresult');
   container.setAttribute("id", itemData.id);
   textArea.setAttribute("class", "comments");
 
-  console.log(itemData.comments[0].content)
+  // console.log(itemData);
   // console.log(itemData.reactions)
   // console.log(itemData.reactions.like)
 
@@ -153,9 +177,14 @@ submitButton.setAttribute('type', 'submit');
   content.textContent = itemData.content;
   likeBtn.textContent = "👍";
   likes.textContent = itemData.reactions.like
+  dislikeBtn.textContent = "👎";
+  dislikes.textContent = itemData.reactions.dislike
+  loveBtn.textContent = "❤️";
+  love.textContent = itemData.reactions.love
   // console.log(itemData)
-  img.src = itemData.giphy[0].source;
+  img.src = itemData.giphy;
   submitButton.textContent = "Submit";
+  submitButton.setAttribute('class', 'submitbuttonpost');
 
   textAreaSection.appendChild(textArea);
   textAreaSection.appendChild(submitButton);
@@ -165,9 +194,21 @@ submitButton.setAttribute('type', 'submit');
   // header.appendChild(headerLink);
   likes.setAttribute("id", `${itemData.id}-post-likes`);
   likeBtn.setAttribute("id", `${itemData.id}-like-btn`);
+  dislikes.setAttribute("id", `${itemData.id}-post-dislikes`);
+  dislikeBtn.setAttribute("id", `${itemData.id}-dislike-btn`);
+  love.setAttribute("id", `${itemData.id}-post-love`);
+  loveBtn.setAttribute("id", `${itemData.id}-love-btn`);
+  likeBtn.setAttribute('class','like');
+  dislikeBtn.setAttribute('class','dislike');
+  loveBtn.setAttribute('class','love');
   emojiDiv.setAttribute("class", "emojiDiv");
   emojiDiv.appendChild(likes);
   emojiDiv.appendChild(likeBtn);
+  emojiDiv.appendChild(dislikes);
+  emojiDiv.appendChild(dislikeBtn);
+  emojiDiv.appendChild(love);
+  emojiDiv.appendChild(loveBtn);
+
 
   //applying borders to the sections
   container.style.border = "2px dotted red"
@@ -258,6 +299,53 @@ function submitFunction(e) {
   parentContainer.appendChild(container);
 
 };
+
+//submit reactions
+function updateReaction(e){
+  //get the like count from frontend
+
+  //append it
+  //write to backend
+
+  e.preventDefault();
+  // let likes = 0;
+    console.log(e);
+    let reaction = "";
+    let likeCount = parseInt(e.path[2].childNodes[2].childNodes[0].textContent);
+    let idname = e.path[2].childNodes[2].childNodes[0].id;
+    let id = idname.slice(0, idname.indexOf('-'));
+    reaction = e.path[2].childNodes[2].childNodes[1].className;
+    console.log(idname, id, reaction);
+    likeCount++;
+    // console.log(likeCount);
+
+    // if(id != "" &&  id != undefined){
+    
+        const url = `http://localhost:5000/posts/${id}/reactions/${reaction}`  // The url for post req to be sent to
+        // console.log(url);
+        let jsonData = {}
+        jsonData.amount = 1;
+        // console.log(jsonData)
+        // jsonData.date = new Date();
+
+      const options = {
+        method: "PUT",
+        // body: JSON.stringify(jsonData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      fetch( url, options )
+      .then( response => response.json() )
+      .catch((err) => {
+        console.log(err);
+      });
+
+      let likeButton = document.getElementById(idname);
+      likeButton.textContent = likeCount.toString();
+    // }
+
+}
 /* <-------------------- Post data to server --------------------->*/
 
 /* <-------------------- error handling --------------------->*/
