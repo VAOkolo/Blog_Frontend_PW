8c0a99ccfb3545b52637e2adcb463816a0ff7528
VAOkolo <vincentaokolo@outlook.com>
2022-07-01 08:45:35 +0100

    refactoring finished

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,131 @@
+//fetch data from database
+async function fetchData() {
+   try{ 
+    fetch(`http://localhost:5000/posts/`)
+        .then(response => response.json())
+        .then(data => appendResults(data));
+   } catch (error) {
+       console.log(error);
+   }
+ }
+
+//loop through fetched posts and append each post to site
+function appendResults(data) {
+  data.forEach(appendResult);
+}
+
+//logic for building out a post container and inserting it into the parent
+function appendResult(itemData) {
+  
+  //postContainer is where all posts will sit
+  const postsContainer = document.querySelector('.parentsection');
+  //container is where all post elements will sit
+  const container = document.createElement('section');
+  const content = document.createElement('p');
+  const img = document.createElement('img');
+  //textarea section and variables - textAreaSection is child of container
+  const textAreaSection = document.createElement('section');
+  const textArea = document.createElement('TEXTAREA');
+  const submitButton = document.createElement('button');
+  //comment section and variables
+  const commentContainer = document.createElement('section');
+  const ul = document.createElement('ul');
+  const commentArray = itemData.comments;
+  //emoji container variables
+  const emojiDiv = document.createElement("section");
+  const likeBtn = document.createElement("button");
+  const likes = document.createElement("p");
+  const dislikeBtn = document.createElement('button');
+  const dislikes = document.createElement("p");
+  const loveBtn = document.createElement('button');
+  const love = document.createElement("p");
+  const likeDiv = document.createElement('div');
+  const dislikeDiv = document.createElement('div');
+  const loveDiv = document.createElement('div');
+
+  //setting key class and id attributes across post container elements
+  container.setAttribute('class','postBody');
+  container.setAttribute('id', itemData.id);
+//   container.setAttribute("id", itemData.id);
+  content.setAttribute('class','postHeaderText');
+  textAreaSection.setAttribute('class','addComment');
+  textArea.setAttribute('class', "comments")
+//   textArea.setAttribute("class", "comments");
+  submitButton.setAttribute('id', `${itemData.id}-submitcomment`)
+  submitButton.setAttribute('type', 'submit');
+  commentContainer.setAttribute('class','commentList');
+  ul.setAttribute('class','commentsListDisplay');
+  commentContainer.setAttribute('id', `${itemData.id}-commentcontainer`);
+  ul.setAttribute('id', `${itemData.id}-commentul`);
+  likes.setAttribute("id", `${itemData.id}-post-likes`);
+  likeBtn.setAttribute("id", `${itemData.id}-like-btn`);
+  dislikes.setAttribute("id", `${itemData.id}-post-dislikes`);
+  dislikeBtn.setAttribute("id", `${itemData.id}-dislike-btn`);
+  love.setAttribute("id", `${itemData.id}-post-love`);
+  loveBtn.setAttribute("id", `${itemData.id}-love-btn`);
+  likeBtn.setAttribute('class','like');
+  dislikeBtn.setAttribute('class','dislike');
+  loveBtn.setAttribute('class','love');
+  //give divs all same class to flex emoji and count elements
+  likeDiv.setAttribute('class',"emojicontainer")
+  dislikeDiv.setAttribute('class',"emojicontainer")
+  loveDiv.setAttribute('class',"emojicontainer")
+  emojiDiv.setAttribute("class", "emojiDiv");
+  
+  // loop through comment array populating ul with lis each with text content from commentArray
+  for(i = 0; i < commentArray.length; i++){
+    let li = document.createElement('li');
+    li.textContent = commentArray[i].content;
+    ul.appendChild(li);
+    li.setAttribute('class','elementCommentsList');
+  }
+  
+  //assign necessary content and other attributes to dom elements
+  img.src = itemData.giphy;
+  submitButton.textContent = "Submit";
+  submitButton.setAttribute('class', 'submitbuttonpost');
+  content.textContent = itemData.content;
+//   content.textContent = itemData.content;
+  likeBtn.textContent = "👍";
+  likes.textContent = itemData.reactions.like
+  dislikeBtn.textContent = "👎";
+  dislikes.textContent = itemData.reactions.dislike
+  loveBtn.textContent = "❤️";
+  love.textContent = itemData.reactions.love
+  
+  //build necessary child divs, and parent post
+  //emoji div
+  emojiDiv.appendChild(likeDiv);
+  emojiDiv.appendChild(dislikeDiv);
+  emojiDiv.appendChild(loveDiv);
+  
+  likeDiv.appendChild(likes)
+  likeDiv.appendChild(likeBtn)
+  dislikeDiv.appendChild(dislikes)
+  dislikeDiv.appendChild(dislikeBtn)
+  loveDiv.appendChild(love)
+  loveDiv.appendChild(loveBtn)
+  //text container
+  textAreaSection.appendChild(textArea);
+  textAreaSection.appendChild(submitButton);
+  //comment container
+  commentContainer.appendChild(ul);
+  //append to parent post
+  container.appendChild(content);
+  container.appendChild(img);
+  container.appendChild(emojiDiv);
+  container.appendChild(textAreaSection);
+  container.appendChild(commentContainer);
+  //append to parent section
+  postsContainer.appendChild(container);
+  
+}
+
 //post comment to backend and print comment on site on submission
 function submitComment(e){
 
     e.preventDefault();
+    console.log(e);
     //get key variables
     let submitButton = e.path[0].id;
     let postid = submitButton.slice(0, submitButton.indexOf('-'))
@@ -41,153 +165,6 @@ function submitComment(e){
       section.appendChild(ul);
     }
   }
-
-//fetch data from database
-async function fetchData() {
-  let response = await fetch(`http://localhost:5000/posts/`);
-  let data = await response.json();
-      appendResults(data);
-  }
-//loop through fetched posts and append each post to site
-function appendResults(data) {
-  data.forEach(appendResult);
-}
-//logic for building out a post container
-function appendResult(itemData) {
-  // console.log(itemData.header);
-  //postContainer is where all posts will sit
-  const postContainer = document.querySelector('.parentsection');
-  //container is where all post elements will sit
-  const container = document.createElement('section');
-  const content = document.createElement('p');
-  const img = document.createElement('img');
-  const textAreaSection = document.createElement('section');
-  const textArea = document.createElement('TEXTAREA');
-  const submitButton = document.createElement('button');
-
-  //comment parts and build
-  const commentContainer = document.createElement('section');
-  commentContainer.setAttribute('id', `${itemData.id}-commentcontainer`);
-  const ul = document.createElement('ul');
-  ul.setAttribute('id', `${itemData.id}-commentul`);
-  const commentArray = itemData.comments;
-  console.log(commentArray);
-
-  // loop through comment array populating uls
-  for(i = 0; i < commentArray.length; i++){
-    let li = document.createElement('li');
-    li.textContent = commentArray[i].content;
-    ul.appendChild(li);
-    li.setAttribute('class','elementCommentsList');
-  }
-  //append ul full of li comments as child element of comment container
-  // console.log(ul);
-  commentContainer.appendChild(ul);
-
-  // container.setAttribute('class', 'searchresult');
-container.setAttribute('id', itemData.id)
-
-//updating dating classes to merge with Daiana's styling
-container.setAttribute('class','postBody');
-content.setAttribute('class','postHeaderText');
-textAreaSection.setAttribute('class','addComment');
-commentContainer.setAttribute('class','commentList');
-ul.setAttribute('class','commentsListDisplay');
-
-
-textArea.setAttribute('class', "comments")
-submitButton.setAttribute('id', `${itemData.id}-submitcomment`)
-submitButton.setAttribute('type', 'submit');
-
-  // console.log(itemData.id)
-  content.textContent = itemData.content;
-
-  const emojiDiv = document.createElement("section");
-  const likeBtn = document.createElement("button");
-  const likes = document.createElement("p");
-  const dislikeBtn = document.createElement('button');
-  const dislikes = document.createElement("p");
-  const loveBtn = document.createElement('button');
-  const love = document.createElement("p");
-  const likeDiv = document.createElement('div');
-  const dislikeDiv = document.createElement('div');
-  const loveDiv = document.createElement('div');
-
-  // container.setAttribute('class', 'searchresult');
-  container.setAttribute("id", itemData.id);
-  textArea.setAttribute("class", "comments");
-
-  // console.log(itemData);
-  // console.log(itemData.reactions)
-  // console.log(itemData.reactions.like)
-
-
-  content.textContent = itemData.content;
-  likeBtn.textContent = "👍";
-  likes.textContent = itemData.reactions.like
-  dislikeBtn.textContent = "👎";
-  dislikes.textContent = itemData.reactions.dislike
-  loveBtn.textContent = "❤️";
-  love.textContent = itemData.reactions.love
-
-  //throw emoji content into respective divs
-  likeDiv.appendChild(likes)
-  likeDiv.appendChild(likeBtn)
-  dislikeDiv.appendChild(dislikes)
-  dislikeDiv.appendChild(dislikeBtn)
-  loveDiv.appendChild(love)
-  loveDiv.appendChild(loveBtn)
-
-  //give divs all same class to flex emoji and count
-  likeDiv.setAttribute('class',"emojicontainer")
-  dislikeDiv.setAttribute('class',"emojicontainer")
-  loveDiv.setAttribute('class',"emojicontainer")
-  // console.log(itemData)
-  img.src = itemData.giphy;
-  submitButton.textContent = "Submit";
-  submitButton.setAttribute('class', 'submitbuttonpost');
-
-  textAreaSection.appendChild(textArea);
-  textAreaSection.appendChild(submitButton);
-
-  // headerLink.href = itemData.url;
-  // headerLink.textContent = itemData.header;
-  // header.appendChild(headerLink);
-  likes.setAttribute("id", `${itemData.id}-post-likes`);
-  likeBtn.setAttribute("id", `${itemData.id}-like-btn`);
-  dislikes.setAttribute("id", `${itemData.id}-post-dislikes`);
-  dislikeBtn.setAttribute("id", `${itemData.id}-dislike-btn`);
-  love.setAttribute("id", `${itemData.id}-post-love`);
-  loveBtn.setAttribute("id", `${itemData.id}-love-btn`);
-  likeBtn.setAttribute('class','like');
-  dislikeBtn.setAttribute('class','dislike');
-  loveBtn.setAttribute('class','love');
-  emojiDiv.setAttribute("class", "emojiDiv");
-  emojiDiv.appendChild(likeDiv);
-  emojiDiv.appendChild(dislikeDiv);
-  emojiDiv.appendChild(loveDiv);
-
-
-  //applying borders to the sections
-  // container.style.border = "2px dotted red"
-
-  //push list items which are comments into ul
-
-  //create a complete post involving all necessary elements
-  // console.log(header, link, text);
-  container.appendChild(content);
-  container.appendChild(img);
-  container.appendChild(emojiDiv);
-  container.appendChild(textAreaSection);
-  container.appendChild(commentContainer);
-  postContainer.appendChild(container);
-}
-
-/* <-------------------- recieve data from server --------------------->*/
-
-/* <-------------------- Post data to server --------------------->*/
-
-
 
 function submitPost(e) {
   e.preventDefault();
--- client/tests/index.test.js
+++ client/tests/index.test.js
@@ -1,31 +1,50 @@
-//for fetch calls
+
+const appFile = require('../js/app');
+// const fs = require('fs');
+// const path = require('path');
+// const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+
 global.fetch = require('jest-fetch-mock');
+// let app;
 
-const functions = require('/Users/vincent/Desktop/Futureproof/Week4/projectWeek/blogFrontendfinal/Blog_Frontend_PW/client/js/app.js');
-
-jest.mock('/Users/vincent/Desktop/Futureproof/Week4/projectWeek/blogFrontendfinal/Blog_Frontend_PW/client/js/app.js');
-
-beforeEach(() => { fetch.resetMocks() })
-
-//TESTING
-
-describe('testing forEach in appendResults', () => {
-    test('accepts json', () => {
-        functions.appendResults = jest.fn()
-        // const testArray = ['this', 'that', 'the other'];
-        // functions.appendResults(testArray);
-        expect(functions.appendResults([1,2,3,4,5])).toHaveBeenCalledTimes(3);
-        })    
-    })
-
-
-describe('testing fetch requests', () => {
-    test('test if fetch is successful', async () => {
-        // await githubHelpers.getGitHubUserInfo('gingertonic')
-        await functions.fetchData()
-        expect(fetch).toHaveBeenCalled()
+describe('testing fetch requst', () => {
+    test("testing fetch data", () => {
+        appFile.fetchData;
+        expect(fetch).toHaveBeenCalledWith('http://localhost:5000/posts/');
     })
 })
+
+// describe('app', () => {
+//     beforeEach(() => {
+//        document.documentElement.innerHTML = html.toString();
+//         app = require('../js/app')
+//     })
+
+//     afterEach(() => {
+//         fetch.resetMocks();
+//     })
+
+//     describe('requests', () => {
+//         describe('getAllPostss', () => {
+//             test('it makes a get request to /posts', () => {
+//                 app.fetchData();
+//                 // expect(fetch).toHaveBeenCalledWith(expect.stringMatching(/\/cats$/))
+//                 expect(fetch.mock.calls[0][0]).toMatch(/posts$/)
+//             })
+//         });
+//     })
+// })
+
+// describe('appendPost', () => {
+//     test('it renders a new post on the page with post data', () => {
+//         const sectionCount = document.querySelectorAll('section').length;
+//         app.submitPost({ name: 'Testy', age: 42 });
+//         const newLiCount = document.querySelectorAll('li').length;
+//         expect(newLiCount).toEqual(liCount + 1)
+//         expect(document.querySelector('section#cats').textContent).toContain("Testy");
+//         expect(document.querySelector('section#cats').textContent).toContain(42);
+//     })
+// })
 
 
 
